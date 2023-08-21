1d4842b1a9355b3e05775192e3f598968c02ea79
matheusflavio <64802910+matheusflavio@users.noreply.github.com>
2023-08-21 11:56:04 -0300

    novo algoritmo de seleção de jogo

--- main.cpp
+++ main.cpp
@@ -5,39 +5,59 @@
 
 using namespace std;
 
-vector<int> sortInstance(int size) {
-  vector<int> numbers(size);
-  for (int i = 0; i < size; i++) {
-    numbers[i] = rand() % 60 + 1;
-    for (int j = 0; j < i; j++) {
-      while (numbers[j] == numbers[i])
-        numbers[i] = rand() % (60 + 1);
-    }
-  }
-  sort(numbers.begin(), numbers.end());
-  return numbers;
-}
-
-void sortAndPrint(int size, int numberOfGames) {
-  vector<int> numbers(size);
-  for (int j = 0; j < numberOfGames; j++) {
-    numbers = sortInstance(size);
-
-    for (auto i : numbers) {
-      cout << i << " ";
-    }
-    cout << endl;
-  }
-}
-
 int main() {
   srand(time(NULL));
 
-  int numberOfGames;
+  // Tamanho de um jogo simples da mega-sena e a quantidade de números possíveis de selecionar
+  int size = 6;
+  int megaSenaOptions = 60;
 
-  cout << "Digite quantos jogos da Mega-Sena quer fazer:" << endl;
-  cin >> numberOfGames;
-  sortAndPrint(6, numberOfGames);
+  // Criando vetor de pares para os 60 números possíveis e os 6 números selecionados para o jogo
+  vector<pair<int,int>> touple(megaSenaOptions, make_pair(0,0)); // O primeiro valor são os números possíveis de marcar na cartela, e o segundo são as ocorrência do número
+  vector<int> numbers;
+
+  // Atualizando o primeiro valor dos pares para refletir os números do possíveis da cartela
+  for (int i = 0; i < megaSenaOptions; i++) {
+    touple[i].first = i + 1;
+  }
+
+  // Variáveis auxiliares para o contador de números que atingiram 60 ocorrências, número sorteado atualmente e quantidade de números sorteados no total
+  int count = 0;
+  int sortedNumber = 0;
+  int sortedNumbersCounter = 0;
+
+  while(count < size) {
+    // Sorteando número de 1 a 60 e atualizando os contadores
+    sortedNumber = rand() % 60 + 1;
+    touple[sortedNumber].second++;
+    sortedNumbersCounter++;
+    
+    // Atualizando valor da contagem para números sorteados ao menos 60 vezes
+    for(auto i : touple) {
+      if (i.second >= 60) {
+        count++;
+      }
+    }
+
+    // Ternário para resetar o contador caso necessário
+    count == 6 ? count = 6 : count = 0;
+  }
+
+  // Adicionando os valores em que há mais de 60 ocorrências para o vetor de números selecionados para o jogo
+  for(auto i : touple) {
+    if(i.second >= 60) {
+      numbers.push_back(i.first);
+    }
+  }
+
+  // Imprimindo os números sorteados para o jogo
+  for (auto i : numbers) {
+    cout << i << " ";
+  }
+  cout << endl;
+
+  // Imprimindo a quantidade total de números que foram sortedos no processo de criação do jogo
+  cout << "Foram sorteados " << sortedNumbersCounter << " numeros no total para realizar esse jogo" << endl;
 
   return 0;
 }
