8097daa0ab53c249512ff13160617c2aa48618a5
Matheus flávio <flavio.matheusgsilva@gmail.com>
2023-08-29 23:12:40 -0300

    Pequenas alterações de escrita

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main() {
 
   // Criando vetor de pares para os 60 números possíveis e os 6 números selecionados para o jogo
   vector<pair<int,int>> touple(megaSenaOptions, make_pair(0,0)); // O primeiro valor são os números possíveis de marcar na cartela, e o segundo são as ocorrência do número
-  vector<int> numbers;
+  vector<int> referalNumbersSelected;
 
   // Atualizando o primeiro valor dos pares para refletir os números do possíveis da cartela
   for (int i = 0; i < megaSenaOptions; i++) {
@@ -43,26 +43,29 @@ int main() {
     count == 6 ? count = 6 : count = 0;
   }
 
-  // Impressão dos pares de números e ocorrências apenas de curiosidade
-  for (auto i : touple) {
-    cout << i.first << " " << i.second << endl;
-  }
-  cout << endl;
-
   // Adicionando os valores em que há mais de 60 ocorrências para o vetor de números selecionados para o jogo
   for(auto i : touple) {
     if(i.second >= 60) {
-      numbers.push_back(i.first);
+      referalNumbersSelected.push_back(i.first);
     }
   }
-  
-  cout << "Jogo sugerido:" << endl;
+
+  for (auto i : touple) {
+    cout << i.first << " " << i.second << endl;
+  }
+
+  cout << endl;
+  cout << "####################################################################" << endl;
+  cout << "A seguir os valores sorteados para o jogo:" << endl;
+
   // Imprimindo os números sorteados para o jogo
-  for (auto i : numbers) {
+  for (auto i : referalNumbersSelected) {
     cout << i << " ";
   }
-  cout << endl << endl;
-
+  cout << endl;
+  cout << "####################################################################" << endl;
+  cout << endl;
+  
   // Imprimindo a quantidade total de números que foram sortedos no processo de criação do jogo
   cout << "Foram sorteados " << sortedNumbersCounter << " numeros no total para realizar esse jogo" << endl;
 
